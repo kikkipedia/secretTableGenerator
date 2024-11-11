1b1e8216f01eb0d8229b2ed4d0a3bcf45905220d
kikkipedia <kristin.akerlund@lir.gu.se>
2024-11-11 14:54:28 +0100

    Added Jonathans code

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,11 +1,33 @@
 <template>
-    <div>
-        Form here
-    </div>
+    <div id="main-window">
+    <div id="title">LIR + JUL + AI</div>
+    <div id="instruction">Fyll i ett adjektiv per konstverk och klicka på Play</div>
+</div>
+<div class="divider-1"></div>
+<div class="art-input-screen">
+<div class="art-input-start">
+    <input type="text" id="artwork1" name="artwork1" placeholder="konstverk #1" required minlength="4" maxlength="12" size="20" />
+</div>
+<div class="art-input">
+    <input type="text" id="artwork2" name="artwork2" placeholder="konstverk #2" required minlength="4" maxlength="12" size="20" />
+</div>
+<div class="art-input">
+    <input type="text" id="artwork3" name="artwork3" placeholder="konstverk #3" required minlength="4" maxlength="12" size="20" />
+</div>
+<div class="art-input">
+    <input type="text" id="artwork4" name="artwork4" placeholder="konstverk #4" required minlength="4" maxlength="12" size="20" />
+</div>
+<div class="art-input-end">
+    <input type="text" id="artwork5" name="artwork5" placeholder="konstverk #5" required minlength="4" maxlength="12" size="20" />
+</div>
+</div>
+<div id="calculate"></div>
+
 </template>
 
 <script setup lang="ts">
 </script>
 
 <style scoped>
+
 </style>
--- src/components/SecretWords.vue
+++ src/components/SecretWords.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div id="main">
         Page with the words from database
     </div>
 </template>
